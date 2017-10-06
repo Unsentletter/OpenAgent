6c71f0cd2e44e8702945d8b4a5939811f77193aa
Tristan Southwell <tristan.southwell@hotmail.com>
2017-10-06 15:00:33 +1100

    init commit

---
+++ assets/javascript/jquery/jquery.js
@@ -0,0 +1,18 @@
+// $('#city_selector_button').on("click", function() {
+//   $('#top_agent_in_city').text("tesdt")
+//   console.log($(this).val());
+// });
+
+// $("button").on("click", function() {
+//   console.log('hit');
+//   $.ajax({
+//     'url': 'http://apidev.openagent.com.au/v1/agentrest/search-by-name/adrian%20bo',
+//     'type': 'GET',
+//     'success': function(data) {
+//       console.log('data', data);
+//     },
+//     'error': function(err) {
+//       console.log('err', err);
+//     }
+//   })
+// });
